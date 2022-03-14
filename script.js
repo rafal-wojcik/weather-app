28b17ec5f661d3ff17ad46c0b470e989143717e1
Rafał Wójcik <rafi.incognito@o2.pl>
2022-03-14 21:17:03 +0100

    feat: rework js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let weather = {
     "apiKey": "f4aef6196d442f0cb2c07fe65da0119e",
     fetchWeather: function(city) {
-       fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey)
+       fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey)
        .then((response) => response.json()) 
     .then((data) => this.displayWeather(data));
     },
